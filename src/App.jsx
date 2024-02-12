1472fd55adba7cc6e68b51e631e3d34baae8bcc9
salargholami <salargholami021@gmail.com>
2024-02-12 21:01:49 +0330

    TodoSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import AddTodoForm from "./components/Todos/AddTodoForm";
 import TodoList from "./components/Todos/TodoList";
-import "./App.css"
+import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
+  const todos = useSelector((state) => state.todos);
+  console.log(todos);
+  
   return (
     <div className="container pt-3">
       <h1 className="text-center text-light">TodoApp with RTK</h1>
